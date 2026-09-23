67619d020f99d252d42a600ce47b4a7107f7af38
doubao <d@d.com>
2026-09-24 02:33:37 +0800

    按leftPan.dylib重写:不禁用系统手势,delegate协调,velocity判方向

--- FullScreenBackTweak.m
+++ FullScreenBackTweak.m
@@ -2,24 +2,12 @@
 #import <objc/runtime.h>
 #import <objc/message.h>
 
-@interface FBSPanGesture : UIPanGestureRecognizer
-@end
-@implementation FBSPanGesture
+@interface FBSGestureDelegate : NSObject <UIGestureRecognizerDelegate>
 @end
 
-@interface FBSDelegate : NSObject <UIGestureRecognizerDelegate>
-+ (instancetype)shared;
-@end
+@implementation FBSGestureDelegate
 
-@implementation FBSDelegate
-+ (instancetype)shared {
-    static FBSDelegate *s;
-    static dispatch_once_t t;
-    dispatch_once(&t, ^{ s = [FBSDelegate new]; });
-    return s;
-}
-
-- (UINavigationController *)navOf:(UIView *)v {
+- (UINavigationController *)navOfView:(UIView *)v {
     UIResponder *r = v.nextResponder;
     while (r) {
         if ([r isKindOfClass:[UINavigationController class]]) return (id)r;
@@ -34,43 +22,51 @@
 
 - (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)g {
     if (![g isKindOfClass:[UIPanGestureRecognizer class]]) return NO;
-    UIPanGestureRecognizer *p = (id)g;
-    UINavigationController *nav = [self navOf:g.view];
+    UIPanGestureRecognizer *pan = (id)g;
+    UINavigationController *nav = [self navOfView:g.view];
     if (!nav || nav.viewControllers.count < 2) return NO;
-    CGPoint t = [p translationInView:g.view];
-    if (t.x < 2) return NO;
-    if (fabs(t.x) < fabs(t.y)) return NO;
+    CGPoint vel = [pan velocityInView:g.view];
+    CGPoint trans = [pan translationInView:g.view];
+    if (fabs(vel.x) < fabs(vel.y)) return NO;
+    if (trans.x < 5) return NO;
     return YES;
 }
 
-- (BOOL)gestureRecognizer:(UIGestureRecognizer *)g shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)o {
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)g1 shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)g2 {
     return NO;
 }
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)g1 shouldBeRequiredToFailByGestureRecognizer:(UIGestureRecognizer *)g2 {
+    return NO;
+}
+
 @end
+
+static FBSGestureDelegate *sharedDelegate = nil;
 
 static void FBSInstallOnNav(UINavigationController *nav) {
     @try {
         for (UIGestureRecognizer *g in nav.view.gestureRecognizers) {
-            if ([g isMemberOfClass:[FBSPanGesture class]]) return;
+            if ([g isMemberOfClass:[UIPanGestureRecognizer class] && g.delegate == sharedDelegate]) return;
         }
         UIGestureRecognizer *sys = nav.interactivePopGestureRecognizer;
         NSArray *targets = [sys valueForKey:@"_targets"];
+        if (targets.count == 0) return;
         id target = targets.firstObject;
         if (!target) return;
-        FBSPanGesture *gesture = [[FBSPanGesture alloc] initWithTarget:target
-                                                               action:NSSelectorFromString(@"handleNavigationTransition:")];
-        gesture.delegate = [FBSDelegate shared];
-        gesture.maximumNumberOfTouches = 1;
-        [nav.view addGestureRecognizer:gesture];
-        sys.enabled = NO;
+        UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc]
+            initWithTarget:target
+                    action:NSSelectorFromString(@"handleNavigationTransition:")];
+        pan.delegate = sharedDelegate;
+        pan.maximumNumberOfTouches = 1;
+        pan.cancelsTouchesInView = YES;
+        [nav.view addGestureRecognizer:pan];
     } @catch (NSException *e) {}
 }
 
 static void FBSWalk(UIViewController *vc) {
     if (!vc) return;
-    if ([vc isKindOfClass:[UINavigationController class]]) {
-        FBSInstallOnNav((id)vc);
-    }
+    if ([vc isKindOfClass:[UINavigationController class]]) FBSInstallOnNav((id)vc);
     for (UIViewController *c in vc.childViewControllers) FBSWalk(c);
 }
 
@@ -85,12 +81,12 @@ static void FBSInstall(void) {
     });
 }
 
-// dylib 入口：加载即执行
 __attribute__((constructor)) static void FBSEntry(void) {
+    static dispatch_once_t once;
+    dispatch_once(&once, ^{ sharedDelegate = [[FBSGestureDelegate alloc] init]; });
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)),
                    dispatch_get_main_queue(), ^{ FBSInstall(); });
     [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidBecomeActiveNotification
                                                       object:nil queue:nil
                                                   usingBlock:^(NSNotification *n){ FBSInstall(); }];
 }
- 
