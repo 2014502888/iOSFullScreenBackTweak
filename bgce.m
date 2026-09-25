552459d6ed45d31e49810093e1f9cad9f5302b3c
050418688 <q2016502@126.com>
2026-09-25 13:03:02 +0800

    test: 空美颜hook

--- bgce.m
+++ bgce.m
@@ -63,30 +63,10 @@ static void WXShowSettings(void) {
 // ==================== 1. 通话美颜 ====================
 static void WXBeauty_Hook(void) {
     Class voipCls = NSClassFromString(@"VoipView");
-    if (!voipCls) voipCls = NSClassFromString(@"MMVoipViewController");
     if (!voipCls) return;
     SEL origSel = @selector(viewDidAppear:);
     Method origMethod = class_getInstanceMethod(voipCls, origSel);
     if (!origMethod) return;
-    __block IMP origImp = method_getImplementation(origMethod);
-    void (^block)(id, BOOL) = ^(id self, BOOL animated) {
-        ((void(*)(id, SEL, BOOL))origImp)(self, origSel, animated);
-        if (!WXGet(kWXKeyBeauty)) return;
-        @try {
-            UIView *view = [(UIViewController *)self view];
-            if (!view) return;
-            for (UIView *sv in view.subviews) { if (sv.tag == 99999) return; }
-            UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
-            btn.frame = CGRectMake(16, 80, 44, 44);
-            btn.layer.cornerRadius = 22;
-            btn.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.4];
-            [btn setTitle:@"美" forState:UIControlStateNormal];
-            [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-            btn.tag = 99999;
-            [view addSubview:btn];
-        } @catch (__unused NSException *e) {}
-    };
-    method_setImplementation(origMethod, imp_implementationWithBlock(block));
 }
 
 static void WXInstall(void) {
