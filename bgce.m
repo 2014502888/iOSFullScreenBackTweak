08d2668fe696ef638671f9638c9bf685ee16e716
050418688 <q2016502@126.com>
2026-09-25 13:00:59 +0800

    test: 只加美颜hook

--- bgce.m
+++ bgce.m
@@ -2,7 +2,6 @@
 #import <objc/runtime.h>
 #import <objc/message.h>
 
-// ==================== 设置存储 ====================
 static NSString *kWXKeyBeauty = @"wx_beauty";
 static NSString *kWXKeyMirror  = @"wx_mirror";
 static NSString *kWXKeyMuteRingtone = @"wx_mute_ring";
@@ -90,73 +89,6 @@ static void WXBeauty_Hook(void) {
     method_setImplementation(origMethod, imp_implementationWithBlock(block));
 }
 
-// ==================== 3. 来电静音铃声 ====================
-static void WXMuteRingtone_Hook(void) {
-    Class playerCls = NSClassFromString(@"AVAudioPlayer");
-    if (!playerCls) return;
-    SEL sel = @selector(play);
-    Method m = class_getInstanceMethod(playerCls, sel);
-    if (!m) return;
-    __block IMP origImp = method_getImplementation(m);
-    void (^block)(id) = ^(id self) {
-        ((void(*)(id, SEL))origImp)(self, sel);
-        if (!WXGet(kWXKeyMuteRingtone)) return;
-        @try { [self setValue:@0.0 forKey:@"volume"]; } @catch (__unused NSException *e) {}
-    };
-    method_setImplementation(m, imp_implementationWithBlock(block));
-}
-
-// ==================== 4. 收藏上锁 ====================
-static void WXFavLock_Hook(void) {
-    Class meCls = NSClassFromString(@"MMSystemSettingViewController");
-    if (!meCls) meCls = NSClassFromString(@"WCTMainPageViewController");
-    if (!meCls) return;
-    SEL sel = @selector(tableView:cellForRowAtIndexPath:);
-    Method m = class_getInstanceMethod(meCls, sel);
-    if (!m) return;
-    __block IMP origImp = method_getImplementation(m);
-    void (^block)(id, UITableView *, NSIndexPath *) = ^(id self, UITableView *tv, NSIndexPath *ip) {
-        UITableViewCell *cell = ((UITableViewCell *(*)(id, SEL, UITableView *, NSIndexPath *))origImp)(self, sel, tv, ip);
-        if (!WXGet(kWXKeyFavLock)) return cell;
-        @try {
-            NSString *text = cell.textLabel.text;
-            if (text && [text containsString:@"收藏"]) { cell.hidden = YES; cell.alpha = 0.0; }
-        } @catch (__unused NSException *e) {}
-        return cell;
-    };
-    method_setImplementation(m, imp_implementationWithBlock(block));
-}
-
 static void WXInstall(void) {
     dispatch_async(dispatch_get_main_queue(), ^{
         WXBeauty_Hook();
-        WXMuteRingtone_Hook();
-        WXFavLock_Hook();
-        for (UIWindowScene *s in [UIApplication sharedApplication].connectedScenes) {
-            if (![s isKindOfClass:[UIWindowScene class]]) continue;
-            for (UIWindow *w in s.windows) {
-                static dispatch_once_t once;
-                dispatch_once(&once, ^{
-                    UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
-                    btn.frame = CGRectMake([UIScreen mainScreen].bounds.size.width - 60,
-                                           [UIScreen mainScreen].bounds.size.height - 220, 44, 44);
-                    btn.layer.cornerRadius = 22;
-                    btn.backgroundColor = [UIColor colorWithRed:0.2 green:0.5 blue:1.0 alpha:0.85];
-                    [btn setTitle:@"微" forState:UIControlStateNormal];
-                    [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-                    btn.titleLabel.font = [UIFont boldSystemFontOfSize:16];
-                    [btn addTarget:[WXBtnTarget shared] action:@selector(onBtn) forControlEvents:UIControlEventTouchUpInside];
-                    [w addSubview:btn];
-                });
-            }
-        }
-    });
-}
-
-__attribute__((constructor)) static void WXConstructor(void) {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)),
-                   dispatch_get_main_queue(), ^{ WXInstall(); });
-    [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidBecomeActiveNotification
-                                                      object:nil queue:nil
-                                                  usingBlock:^(NSNotification *n){ WXInstall(); }];
-}
