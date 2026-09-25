e4942e18eb7cf5815f812a4accc15b5d95360281
050418688 <q2016502@126.com>
2026-09-25 13:28:18 +0800

    bgce: 完整4个hook(美颜/镜像/静音/收藏上锁)

--- bgce.m
+++ bgce.m
@@ -62,17 +62,47 @@ static void WXShowSettings(void) {
 
 static void WXInstall(void) {
     dispatch_async(dispatch_get_main_queue(), ^{
-        // === 1. 通话美颜(暂不启用) ===
-
-        // === 2. 通话镜像 ===
         Class voipCls = NSClassFromString(@"VoipView");
         if (!voipCls) voipCls = NSClassFromString(@"MMVoipViewController");
+
+        // === 1. 通话美颜 ===
+        if (voipCls) {
+            Method m = class_getInstanceMethod(voipCls, @selector(viewDidAppear:));
+            if (m) {
+                __block IMP orig = method_getImplementation(m);
+                method_setImplementation(m, imp_implementationWithBlock(^(id self, BOOL animated) {
+                    ((void(*)(id, SEL, BOOL))orig)(self, @selector(viewDidAppear:), animated);
+                    if (!WXGet(kWXKeyBeauty)) return;
+                    @try {
+                        UIView *v = [(UIViewController *)self view];
+                        if (!v) return;
+                        for (UIView *sv in v.subviews) { if (sv.tag == 99999) return; }
+                        UIButton *b = [UIButton buttonWithType:UIButtonTypeSystem];
+                        b.frame = CGRectMake(16, 80, 44, 44);
+                        b.layer.cornerRadius = 22;
+                        b.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.4];
+                        [b setTitle:@"美" forState:UIControlStateNormal];
+                        [b setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+                        b.tag = 99999;
+                        [v addSubview:b];
+                    } @catch (__unused NSException *e) {}
+                }));
+            }
+        }
+
+        // === 2. 通话镜像 ===
         if (voipCls) {
             Method m = class_getInstanceMethod(voipCls, @selector(layoutSubviews));
             if (m) {
                 __block IMP orig = method_getImplementation(m);
                 method_setImplementation(m, imp_implementationWithBlock(^(id self) {
                     ((void(*)(id, SEL))orig)(self, @selector(layoutSubviews));
+                    if (!WXGet(kWXKeyMirror)) return;
+                    @try {
+                        for (UIView *sv in [(UIView *)self subviews]) {
+                            sv.transform = CGAffineTransformMakeScale(-1.0, 1.0);
+                        }
+                    } @catch (__unused NSException *e) {}
                 }));
             }
         }
@@ -91,7 +121,24 @@ static void WXInstall(void) {
             }
         }
 
-        // === 4. 收藏上锁(暂不启用) ===
+        // === 4. 收藏上锁 ===
+        Class meCls = NSClassFromString(@"MMSystemSettingViewController");
+        if (!meCls) meCls = NSClassFromString(@"WCTMainPageViewController");
+        if (meCls) {
+            Method m = class_getInstanceMethod(meCls, @selector(tableView:cellForRowAtIndexPath:));
+            if (m) {
+                __block IMP orig = method_getImplementation(m);
+                method_setImplementation(m, imp_implementationWithBlock(^UITableViewCell *(id self, UITableView *tv, NSIndexPath *ip) {
+                    UITableViewCell *cell = ((UITableViewCell *(*)(id, SEL, UITableView *, NSIndexPath *))orig)(self, @selector(tableView:cellForRowAtIndexPath:), tv, ip);
+                    if (!WXGet(kWXKeyFavLock)) return cell;
+                    @try {
+                        NSString *t = cell.textLabel.text;
+                        if (t && [t containsString:@"收藏"]) { cell.hidden = YES; cell.alpha = 0.0; }
+                    } @catch (__unused NSException *e) {}
+                    return cell;
+                }));
+            }
+        }
 
         // 悬浮按钮
         for (UIWindowScene *s in [UIApplication sharedApplication].connectedScenes) {
