72ac4e36ce8ead18453a753bbdcf5ba45c517a17
050418688 <q2016502@126.com>
2026-09-25 12:56:42 +0800

    test: 最简版确认能编译

--- bgce.m
+++ bgce.m
@@ -1,7 +1,6 @@
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
 #import <objc/message.h>
-#import <AVFoundation/AVFoundation.h>
 
 // ==================== 设置存储 ====================
 static NSString *kWXKeyBeauty = @"wx_beauty";
@@ -19,7 +18,6 @@ static void WXSet(NSString *key, BOOL val) {
     [[NSUserDefaults standardUserDefaults] setBool:val forKey:key];
 }
 
-// ==================== 悬浮设置按钮 ====================
 static UIViewController *WXTopVC(void) {
     UIWindowScene *ws = nil;
     for (UIWindowScene *s in [UIApplication sharedApplication].connectedScenes) {
@@ -35,22 +33,16 @@ static void WXShowSettings(void) {
     UIViewController *top = WXTopVC();
     if (!top) return;
     UIAlertController *ac = [UIAlertController alertControllerWithTitle:@"微信增强设置" message:nil preferredStyle:UIAlertControllerStyleActionSheet];
-
     BOOL be = WXGet(kWXKeyBeauty);
     [ac addAction:[UIAlertAction actionWithTitle:be ? @"✓ 通话美颜: 开" : @"  通话美颜: 关" style:UIAlertActionStyleDefault handler:^(UIAlertAction *a){ WXSet(kWXKeyBeauty, !be); }]];
-
     BOOL mi = WXGet(kWXKeyMirror);
     [ac addAction:[UIAlertAction actionWithTitle:mi ? @"✓ 通话镜像: 开" : @"  通话镜像: 关" style:UIAlertActionStyleDefault handler:^(UIAlertAction *a){ WXSet(kWXKeyMirror, !mi); }]];
-
     BOOL mu = WXGet(kWXKeyMuteRingtone);
     [ac addAction:[UIAlertAction actionWithTitle:mu ? @"✓ 来电静音铃声: 开" : @"  来电静音铃声: 关" style:UIAlertActionStyleDefault handler:^(UIAlertAction *a){ WXSet(kWXKeyMuteRingtone, !mu); }]];
-
     BOOL fl = WXGet(kWXKeyFavLock);
     [ac addAction:[UIAlertAction actionWithTitle:fl ? @"✓ 收藏上锁: 开" : @"  收藏上锁: 关" style:UIAlertActionStyleDefault handler:^(UIAlertAction *a){ WXSet(kWXKeyFavLock, !fl); }]];
-
     BOOL qe = WXGet(kWXKeyQuickEdit);
     [ac addAction:[UIAlertAction actionWithTitle:qe ? @"✓ 快捷发送编辑图片: 开" : @"  快捷发送编辑图片: 关" style:UIAlertActionStyleDefault handler:^(UIAlertAction *a){ WXSet(kWXKeyQuickEdit, !qe); }]];
-
     [ac addAction:[UIAlertAction actionWithTitle:@"关闭" style:UIAlertActionStyleCancel handler:nil]];
     [top presentViewController:ac animated:YES completion:nil];
 }
@@ -69,118 +61,8 @@ static void WXShowSettings(void) {
 - (void)onBtn { WXShowSettings(); }
 @end
 
-// ==================== 1. 通话美颜 ====================
-static void WXBeauty_Hook(void) {
-    Class voipCls = NSClassFromString(@"VoipView");
-    if (!voipCls) voipCls = NSClassFromString(@"MMVoipViewController");
-    if (!voipCls) return;
-
-    SEL origSel = @selector(viewDidAppear:);
-    Method origMethod = class_getInstanceMethod(voipCls, origSel);
-    if (!origMethod) return;
-
-    __block IMP origImp = method_getImplementation(origMethod);
-    void (^block)(id, BOOL) = ^(id self, BOOL animated) {
-        ((void(*)(id, SEL, BOOL))origImp)(self, origSel, animated);
-        if (!WXGet(kWXKeyBeauty)) return;
-        @try {
-            UIView *view = [(UIViewController *)self view];
-            if (!view) return;
-            for (UIView *sv in view.subviews) {
-                if (sv.tag == 99999) return;
-            }
-            UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
-            btn.frame = CGRectMake(16, 80, 44, 44);
-            btn.layer.cornerRadius = 22;
-            btn.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.4];
-            [btn setTitle:@"美" forState:UIControlStateNormal];
-            [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-            btn.tag = 99999;
-            [view addSubview:btn];
-        } @catch (__unused NSException *e) {}
-    };
-    IMP newImp = imp_implementationWithBlock(block);
-    method_setImplementation(origMethod, newImp);
-}
-
-// ==================== 2. 通话镜像 ====================
-static void WXMirror_Hook(void) {
-    Class voipCls = NSClassFromString(@"VoipView");
-    if (!voipCls) return;
-    SEL sel = @selector(layoutSubviews);
-    Method m = class_getInstanceMethod(voipCls, sel);
-    if (!m) return;
-    __block IMP origImp = method_getImplementation(m);
-    void (^block)(id) = ^(id self) {
-        ((void(*)(id, SEL))origImp)(self, sel);
-        if (!WXGet(kWXKeyMirror)) return;
-        @try {
-            for (UIView *sv in [(UIView *)self subviews]) {
-                sv.transform = CGAffineTransformMakeScale(-1.0, 1.0);
-            }
-        } @catch (__unused NSException *e) {}
-    };
-    IMP newImp = imp_implementationWithBlock(block);
-    method_setImplementation(m, newImp);
-}
-
-// ==================== 3. 来电静音铃声 ====================
-static void WXMuteRingtone_Hook(void) {
-    Class playerCls = NSClassFromString(@"AVAudioPlayer");
-    if (!playerCls) return;
-    SEL sel = @selector(play);
-    Method m = class_getInstanceMethod(playerCls, sel);
-    if (!m) return;
-    __block IMP origImp = method_getImplementation(m);
-    void (^block)(id) = ^(id self) {
-        ((void(*)(id, SEL))origImp)(self, sel);
-        if (!WXGet(kWXKeyMuteRingtone)) return;
-        @try { [self setValue:@0.0 forKey:@"volume"]; } @catch (__unused NSException *e) {}
-    };
-    IMP newImp = imp_implementationWithBlock(block);
-    method_setImplementation(m, newImp);
-}
-
-// ==================== 4. 收藏上锁 ====================
-static void WXFavLock_Hook(void) {
-    Class meCls = NSClassFromString(@"MMSystemSettingViewController");
-    if (!meCls) meCls = NSClassFromString(@"WCTMainPageViewController");
-    if (!meCls) return;
-    SEL sel = @selector(tableView:cellForRowAtIndexPath:);
-    Method m = class_getInstanceMethod(meCls, sel);
-    if (!m) return;
-    __block IMP origImp = method_getImplementation(m);
-    void (^block)(id, UITableView *, NSIndexPath *) = ^(id self, UITableView *tv, NSIndexPath *ip) {
-        UITableViewCell *cell = ((UITableViewCell *(*)(id, SEL, UITableView *, NSIndexPath *))origImp)(self, sel, tv, ip);
-        if (!WXGet(kWXKeyFavLock)) return cell;
-        @try {
-            NSString *text = cell.textLabel.text;
-            if (text && [text containsString:@"收藏"]) {
-                cell.hidden = YES;
-                cell.alpha = 0.0;
-            }
-        } @catch (__unused NSException *e) {}
-        return cell;
-    };
-    IMP newImp = imp_implementationWithBlock(block);
-    method_setImplementation(m, newImp);
-}
-
-// ==================== 5. 快捷发送编辑图片 ====================
-static void WXQuickEdit_Hook(void) {
-    Class pickerCls = NSClassFromString(@"MMImagePickerController");
-    if (!pickerCls) return;
-}
-
-// ==================== 安装 ====================
 static void WXInstall(void) {
     dispatch_async(dispatch_get_main_queue(), ^{
-        WXBeauty_Hook();
-        WXMirror_Hook();
-        WXMuteRingtone_Hook();
-        WXFavLock_Hook();
-        WXQuickEdit_Hook();
-
         for (UIWindowScene *s in [UIApplication sharedApplication].connectedScenes) {
             if (![s isKindOfClass:[UIWindowScene class]]) continue;
             for (UIWindow *w in s.windows) {
